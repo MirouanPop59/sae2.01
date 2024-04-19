fd7a15efa25ee1b1fe7d76c3ee394183c97f4080
sven.guillain <sven.guillain@gmail.com>
2024-04-19 10:47:14 +0200

    Je suis un nouveau commit test

--- src/main/java/org/uphf/sae/HelloApplication.java
+++ src/main/java/org/uphf/sae/HelloApplication.java
@@ -3,18 +3,18 @@ package org.uphf.sae;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        BorderPane root = new BorderPane();
+        primaryStage.setScene(new Scene(root, 140, 85));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
