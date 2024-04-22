e26d9413513e58743a37aa907f52804e90f3c255
alexandre.dulas <dulasalexandre@gmail.com>
2024-04-22 16:36:33 +0200

    avancée des classes Mine et Entrepot (Alexandre Dulas)

---
+++ src/main/java/org/uphf/sae/Entrepot.java
@@ -0,0 +1,45 @@
+package org.uphf.sae;
+
+
+public class Entrepot extends Secteur {
+    private int idEntrepot;
+    private int ligne;
+    private int colonne;
+    private String typeM;
+    protected int nbM;
+
+    public Entrepot(int idEntrepot,int ligne, int colonne,String typeM) {
+        this.idEntrepot = idEntrepot;
+        this.ligne=ligne;
+        this.colonne = colonne;
+        this.typeM=typeM;
+        this.nbM=0;
+
+    }
+
+
+
+    public String TypeMineraiE() {
+        return typeM;
+    }
+
+    public int getIdEntrepot() {
+        return idEntrepot;
+    }
+
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+
+    public int getNbM() {
+        return nbM;
+    }
+
+    public String toString(){
+        return "E"+getIdEntrepot()+" | "+typeM+" | "+getNbM();
+    }
+}
---
+++ src/main/java/org/uphf/sae/Mine.java
@@ -0,0 +1,55 @@
+package org.uphf.sae;
+import java.util.*;
+
+public class Mine extends Secteur {
+    private int idMine;
+    private int ligne;
+    private int colonne;
+    private String typeM;
+    protected int nbMinerai;
+    private int CapaInitial;
+
+    public Mine(int idMine, int ligne, int colonne, String typeM) {
+        this.idMine = idMine;
+        this.ligne = ligne;
+        this.colonne = colonne;
+        this.typeM = typeM;
+        this.CapaInitial = genererInt(50,100);
+        this.nbMinerai = this.CapaInitial;
+
+    }
+
+    public int genererInt(int borneInf,int borneSup){
+        Random random=new Random();
+        int nb;
+        nb=borneInf+random.nextInt(borneSup-borneInf);
+        return nb;
+    }
+
+    public void BaisserNbMinerais() {
+    }
+
+    public String TypeMineraiM() {
+        return typeM;
+    }
+
+    public int getIdMine() {
+        return idMine;
+    }
+
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+
+    public int getCapaInitial() {
+        return CapaInitial;
+    }
+
+    public int getNbMinerai() {
+        return this.nbMinerai;
+    }
+}
