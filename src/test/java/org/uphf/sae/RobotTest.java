e03bfcd0e82a467ccff9995db30ee87dc79679ba
alexandre <dulasalexandre@gmail.com>
2024-05-27 19:21:01 +0200

    junit terminé

--- src/test/java/org/uphf/sae/RobotTest.java
+++ src/test/java/org/uphf/sae/RobotTest.java
@@ -75,8 +75,50 @@ class RobotTest {
         assertEquals(6, robot.getLigne());
     }
 
+    @Test
+    void avancerHorsLimiteNord() {
+        robot.setColonne(0); // Positionner le robot à la limite nord
+        robot.avancer(monde, "N");
+        assertEquals(0, robot.getColonne()); // Le robot ne doit pas sortir de la grille
+    }
+
+    @Test
+    void avancerHorsLimiteSud() {
+        robot.setColonne(9); // Positionner le robot à la limite sud
+        robot.avancer(monde, "S");
+        assertEquals(9, robot.getColonne()); // Le robot ne doit pas sortir de la grille
+    }
+
+    @Test
+    void avancerHorsLimiteOuest() {
+        robot.setLigne(0); // Positionner le robot à la limite ouest
+        robot.avancer(monde, "O");
+        assertEquals(0, robot.getLigne()); // Le robot ne doit pas sortir de la grille
+    }
+
+    @Test
+    void avancerHorsLimiteEst() {
+        robot.setLigne(9); // Positionner le robot à la limite est
+        robot.avancer(monde, "E");
+        assertEquals(9, robot.getLigne()); // Le robot ne doit pas sortir de la grille
+    }
+
+    @Test
+    void collecter() {
+        robot.setColonne(5);
+        robot.setLigne(5);
+        mine = new Mine(1, 5, 6, "OR"); // Ajout d'une mine avec des minerais
+        robot.recolter(this.mine);
+        assertEquals(2, robot.getNbM());
+    }
+
+    @Test
+    void deposer() {
+        robot.setColonne(5);
+        robot.setLigne(4);
+        robot.deposer(entrepot);
+        assertEquals(0, robot.getNbM()); // Le robot doit avoir déposé tous ses minerais
 
 
-
-
+    }
 }
