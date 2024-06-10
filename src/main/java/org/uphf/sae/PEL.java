e38e7b3e2a28d5de82652eb73b1ce78c365b6bf1
MirouanPop59 <156205461+MirouanPop59@users.noreply.github.com>
2024-06-10 12:01:48 +0200

    Update PEL.java

--- src/main/java/org/uphf/sae/PEL.java
+++ src/main/java/org/uphf/sae/PEL.java
@@ -3,21 +3,21 @@ package org.uphf.sae;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class PEL {
+public class Pel {
 
     // NOTE :Traduction d'un algo par le chat, ne pas utiliser comme tel !!!
-
-    static void PEL(int[][] G, int s) {
+//test
+    static void Pel (int[][] G, int s) {
         int numSommets = G.length;
 
         // Marquer tous les sommets comme non visités (par défaut)
-        boolean[] marqués = new boolean[numSommets];
+        boolean[] marques = new boolean[numSommets];
 
         // File pour le parcours BFS
         Queue<Integer> file = new LinkedList<>();
 
         // Marquer le sommet source et l'enfiler
-        marqués[s] = true;
+        marques[s] = true;
         file.add(s);
 
         while (!file.isEmpty()) {
@@ -27,31 +27,12 @@ public class PEL {
 
             // Vérifier tous les voisins du sommet défiler
             for (int t = 0; t < numSommets; t++) {
-                if (G[s][t] == 1 && !marqués[t]) { // G[s][t] == 1 signifie qu'il y a une arête entre s et t
-                    marqués[t] = true;
+                if (G[s][t] == 1 && !marques[t]) { // G[s][t] == 1 signifie qu'il y a une arête entre s et t
+                    marques[t] = true;
                     file.add(t);
                 }
             }
         }
-    }
-
-   public static void main(String[] args) {
-        // Représentation du graphe par une matrice d'adjacence 10x10
-        int[][] G = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0,0 , 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        System.out.println("Parcours en largeur à partir du sommet 2:");
-        PEL(G, 2);
     }
 }
 
